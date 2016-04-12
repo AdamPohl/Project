4da55bb0ed0c8ef5e7576dc814f0df09fa8f0f93
Huaraz2 <adam.pohl9999@gmail.com>
2016-04-12 10:51:46 +0100

    Adding place names :smiling_imp:

--- code.R
+++ code.R
@@ -1,10 +1,10 @@
-data1 <- scan(file="/home/huaraz2/Desktop/multivarite/Project/project_heart_cleveland.txt")
+data1 <- scan(file="project_heart_cleveland.txt")
 mat1 <- matrix(data1, ncol=75, byrow=TRUE)
-data2 <- scan(file="/home/huaraz2/Desktop/multivarite/Project/project_heart_hungarian.txt")
+data2 <- scan(file="project_heart_hungarian.txt")
 mat2 <- matrix(data2, ncol=75, byrow=TRUE)
-data3 <- scan(file="/home/huaraz2/Desktop/multivarite/Project/project_heart_longbeachva.txt")
+data3 <- scan(file="project_heart_longbeachva.txt")
 mat3 <- matrix(data3, ncol=75, byrow=TRUE)
-data4 <- scan(file="/home/huaraz2/Desktop/multivarite/Project/project_heart_switzerland.txt")
+data4 <- scan(file="project_heart_switzerland.txt")
 mat4 <- matrix(data4, ncol=75, byrow=TRUE)
 
 vec1 <- vector()
@@ -13,18 +13,24 @@ vec3 <- vector()
 vec4 <- vector()
 
 for (i in 1:282){
-  vec1[i] <- "one"
+  vec1[i] <- "Cleveland"
 }
 mat1 <- cbind(mat1, vec1)
 for (i in 1:294){
-  vec2[i] <- "two"
+  vec2[i] <- "Hungary"
 }
 mat2 <- cbind(mat2, vec2)
 for (i in 1:200){
-  vec3[i] <- "three"
+  vec3[i] <- "Longbeach"
 }
 mat3 <- cbind(mat3, vec3)
 for (i in 1:123){
-  vec4[i] <- "four"
+  vec4[i] <- "Switzerland"
 }
 mat4 <- cbind(mat4, vec4)
+
+mat = rbind(mat1, mat2, mat3, mat4)
+
+names =  c("id", "ccf", "age", "sex", "painloc", "painexer", "relrest", "pncaden", "cp", "trestbps", "htn", "chol", "smoke", "cigs", "years", "fbs", "dm", "famhist", "restecg", "ekgmo", "ekgday", "ekgyr", "dig", "prop", "nitr", "pro", "diuretic", "proto", "thaldur", "thaltime", "met", "thalach", "thalrest", "tpeakbps", "tpeakbpd", "dummy", "trestbpd", "exang", "xhypo", "oldpeak", "slope", "rldv5", "rldv5e", "ca", "restckm", "exerckm", "restef", "restwm", "exeref", "exerwm", "thal", "thalsev", "thalpul", "earlobe", "cmo", "cday", "cyr", "num", "lmt", "ladprox", "laddist", "diag", "cxmain", "ramus", "om1", "om2", "rcaprox", "rcadist", "lvx1", "lvx2", "lvx3", "lvx4", "lvf", "cathef", "junk", "place")
+
+colnames(mat) = names
